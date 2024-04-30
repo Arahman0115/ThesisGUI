d33c1db2669b216efaddfdd0f1f98c03d32e561c
Arahman0115 <ahnaf.rahman306@gmail.com>
2024-04-30 19:37:43 -0400

    Removing the pers data

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ahnaf\OneDrive\Desktop\ThesisGUI-master\myenv
+set VIRTUAL_ENV=C:\Users\ahnaf\Thesis stuff\ThesisGUI-master\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
